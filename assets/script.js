fbca91c86d1e101ad506f610b99aff0d4ae04817
Burke <beatty.burke@gmail.com>
2021-04-17 13:02:18 -0400

    added alerts for not a number entry

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,28 @@
-var student = document.getElementById("student-names");
-var grade = document.getElementById("grades");
-var comment = document.getElementById("msg");
-var saveButton = document.getElementById("save");
-var savedName = document.getElementById("saved-name");
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
+var random =""
+var newPass = "";
 
-function saveLastGrade() {
-  // Save related form data as an object
-  var studentGrade = {
-    student: student.value,
-    grade: grade.value,
-    comment: comment.value.trim()
-  };
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-  localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
+
+function generatePassword() {
+    var pass = prompt('Enter the number of characters for desired password length between 8 and 128.')
+
+    if (pass < 8 || pass > 128) {
+        alert('Invalid! Try Again!')
+        return generatePassword ();
+    }
+    if (isNaN(pass)) {
+        alert("That is not a number! Please choose a number between 8 and 128!")
+        return generatePassword();
+    }
+  return 'yourpassword';
 }
-
-function renderLastGrade() {
-  // Use JSON.parse() to convert text to JavaScript object
-  var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-  // Check if data is returned, if not exit out of the function
-  if (lastGrade !== null) {
-  document.getElementById("saved-name").innerHTML = lastGrade.student;
-  document.getElementById("saved-grade").innerHTML = lastGrade.grade;
-  document.getElementById("saved-comment").innerHTML = lastGrade.comment;
-  } else {
-    return;
-  }
+// Write password to the #password input
+function writePassword() {
+  console.log('writing password');
+  var password o= generatePassword();
+  var passwordText = document.querySelector('#password');
+  passwordText.value = password;
 }
-
-saveButton.addEventListener("click", function(event) {
-event.preventDefault();
-saveLastGrade();
-renderLastGrade();
-});
-
-// The init() function fires when the page is loaded 
-function init() {
-  // When the init function is executed, the code inside renderLastGrade function will also execute
-  renderLastGrade();
-}
-init();
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
